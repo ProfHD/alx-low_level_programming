98a6d152381d106cec3642a48f95279731c120fe
madara M <ephraimmaboa@gmail.com>
2022-10-15 06:31:59 -0700

    6-size.c correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@ int main(void)
 	printf("Size of int: %i byte(s)\n", sizeof(int));
 	printf("Size of long int: %i byte(s)\n", sizeof(long int));
 	printf("Size of long long int: %i byte(s)\n", sizeof(long long int));
-	printf("Size of float:%i byte(s)\n", sizeof(float));
+	printf("Size of float: %i byte(s)\n", sizeof(float));
 	return (0);
 }
 
