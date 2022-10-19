dc4bcc17133f7f5777cecc727f44b50084155014
madara M <ephraimmaboa@gmail.com>
2022-10-18 23:36:20 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+void print_alphbet(void);
+void print_alphbet_x10(void);
+int _islower(int c);
+int _isalpha(int );
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+int _putchar(char c);
+
+#endif
